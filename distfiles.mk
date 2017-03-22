461051f5962426d43f0e0f3c8e8af7102fe5b0bc
Peter Ragone <pcragone@gmail.com>
2017-03-22 17:22:29 -0400

    Added Vision ZL7431 in-wall single relay switch

--- distfiles.mk
+++ distfiles.mk
@@ -327,6 +327,7 @@ DISTFILES =	.gitignore \
 	config/vision/zd2301.xml \
 	config/vision/zf5201.xml \
 	config/vision/zg8101.xml \
+	config/vision/zl7431.xml \
 	config/vision/zm1601eu.xml \
 	config/vision/zm1601eu5.xml \
 	config/vision/zm1602eu.xml \
